0d1def51e552f1eebfa1fa7589671e2559b36c01
wayne309 <wayne@nuklabs.com>
2023-01-06 18:47:14 +0900

    hello

--- pages/game.js
+++ pages/game.js
@@ -266,7 +266,13 @@ export default function Game({
 				/>
 				<link rel="icon" href="/favicon.ico" />
 
+{/*
 				<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+		*/}
+
+
+				
+  
 			</Head>
 
 
