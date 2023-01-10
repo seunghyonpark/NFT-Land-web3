b9a4e605d765087adc898bad8a726e80058bfceb
wayne309 <wayne@nuklabs.com>
2023-01-10 14:15:28 +0900

    aaa

--- components/SelectedCard/index.jsx
+++ components/SelectedCard/index.jsx
@@ -305,12 +305,16 @@ bg-cover bg-center bg-[url('/img_tomo.png')]
 		
 		lg:pl-[108px] lg:pr-[100px] lg:pt-[50px]
 
-		xl:pl-[120px] xl:pr-[85px] xl:pt-[70px]
+		xl:pl-[165px] xl:pr-[0px] xl:pt-[130px]
 
 		
 		
 		
 		">
+
+			{/*
+			xl:pl-[165px] xl:pr-[0px] xl:pt-[130px]
+			*/}
 
 <div className="
 	flex
