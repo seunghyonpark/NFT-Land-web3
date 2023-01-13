f80ffc62b79f80c0bf64f92f8980b449374d12f5
wayne309 <wayne@nuklabs.com>
2023-01-13 12:56:05 +0900

    og image change

--- pages/yangjae.jsx
+++ pages/yangjae.jsx
@@ -379,7 +379,7 @@ Based on concrete technology and moves, broad networks with industries, and soli
 Sunmiya Club is one representative IP of Web 3.0 era, expanding with our own universe and collaborating with various established industries and metaverse IPs.
 					">
 				</meta>
-				<meta property="og:image" content="https://gdx.nuklabs.xyz/api/og?t=1234"></meta>
+				<meta property="og:image" content="https://gdx.nuklabs.xyz/api/og?t=12349458240343850"></meta>
 				<meta proterty="og:url"
 					content="https://gdx.nuklabs.xyz/gangnam">
 				</meta>
