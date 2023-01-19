a70996edf062446a3ea1db7834ea901583bf7919
wayne309 <wayne@nuklabs.com>
2023-01-19 23:30:17 +0900

    hello

--- clients/livepeer.ts
+++ clients/livepeer.ts
@@ -3,7 +3,9 @@ import { studioProvider } from "livepeer/providers/studio";
 import { LIVEPEER_KEY } from "../constants";
 
 const LivePeerClient = createReactClient({
-  provider: studioProvider({ apiKey: LIVEPEER_KEY }),
+  provider: studioProvider({
+    apiKey: LIVEPEER_KEY
+  }),
 });
 
 export default LivePeerClient;
