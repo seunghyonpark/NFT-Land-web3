147801f3f75681c2d4a20b7d34181696fda48e11
wayne309 <wayne@nuklabs.com>
2023-01-20 17:13:18 +0900

    uppppp

--- components/upload/VideoInput.tsx
+++ components/upload/VideoInput.tsx
@@ -1,13 +1,21 @@
 import React, { useRef, useState } from "react";
 
-export default function UploadInput(props: any) {
+export default function UploadInput(props) {
+  
+  
   const [video, setVideo] = useState("");
+
+
   const videoRef = useRef(null);
 
-  const handleInput = (e: any) => {
-    setVideo(URL.createObjectURL(e.target.files[0]));
+  const handleInput = (e) => {
+
+    setVideo( URL.createObjectURL(e.target.files[0]) );
+
     props.setVideo(e.target.files[0]);
+
     console.log(e.target.files[0]);
+
   };
 
  
--- constants/index.ts
+++ constants/index.ts
@@ -1,14 +1,14 @@
 const APP_NAME = 'OurTube'
 const APP_DESCRIPTION = 'A decentralized video sharing platform'
 const APP_VERSION = '0.0.1'
-const CONTRACT_ADDRESS = '0xEd2e212B8191827cd27DcB60A09BA5F789eD78e2'
+const CONTRACT_ADDRESS = '0xcbA2a9E8bA825FB7C98351AD99cc745Fd393DAa8'
 const IPFS_GATEWAY = 'https://w3s.link/ipfs/'
 const IMAGEKIT_URL = 'https://ik.imagekit.io/' + process.env.NEXT_PUBLIC_IMAGEKIT_ID + '/'
 const LIVEPEER_KEY = process.env.NEXT_PUBLIC_LIVEPEER_KEY
 const WEB3_STORAGE_URL = 'https://api.web3.storage/upload'
 const WEB3_STORAGE_KEY = process.env.NEXT_PUBLIC_WEB3_STORAGE_KEY
 const SUB_GRAPH_BASE_URL = 'https://api.thegraph.com'
-const SUB_GRAPH_NAME = 'suhailkakar/ourtube-v2'
+const SUB_GRAPH_NAME = 'seunghyonpark/beauty'
 const SUB_GRAPH_URL = `${SUB_GRAPH_BASE_URL}/subgraphs/name/${SUB_GRAPH_NAME}`
 
 export {
